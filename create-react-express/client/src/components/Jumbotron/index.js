70cf82fa58f81b866591ad835c4fafc09462343f
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-22 19:01:50 -0800

    Style jumbotron

--- create-react-express/client/src/components/Jumbotron/index.js
+++ create-react-express/client/src/components/Jumbotron/index.js
@@ -21,18 +21,23 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={0}>
         <Grid item md={12}>
-        <img className={classes.img} alt="complex" src="https://images.pexels.com/photos/415071/pexels-photo-415071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-
+          <div>
+            <img id="jumbotron" className={classes.img} alt="complex" src="https://images.pexels.com/photos/415071/pexels-photo-415071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+            <div id="jumboText">
+              <h1>Google Books</h1>
+              <h3>Search and save your favorite books</h3>
+            </div>
+          </div>
           {/* <Paper className={classes.paper}> 
           </Paper> */}
-          
-        </Grid>
-        
-      </Grid>
-      
 
-      
-    </div>
+        </Grid>
+
+      </Grid>
+
+
+
+    </div >
   );
 }
 
--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL: "https://www.googleapis.com/books/v1/volumes?q=";
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export default {
 //Search for books
